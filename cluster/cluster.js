4860997e07de8f5d3b3e89dbb65210bd016218aa
CaXaPeK <sasha.bazdyrev2004@gmail.com>
2023-03-30 18:43:26 +0700

    Сделал массив кругов и чтобы круги не накладывались

--- cluster/cluster.js
+++ cluster/cluster.js
@@ -2,6 +2,9 @@ var canvas = document.getElementById("fieldCanvas");
 canvas.width = 500;
 canvas.height = 500;
 
+let circlesX = [];
+let circlesY = [];
+
 function getPosition(event) {
     var posX = 0;
     var posY = 0;
@@ -32,13 +35,31 @@ function drawCircle(x, y) {
     circle.fill();
 }
 
+function circlesDistance(x1, y1, x2, y2) {
+    return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) - 10 * 2;
+}
+
+function isNoCirclesNearby(x, y) {
+    for (let i = 0; i < circlesX.length; i++) {
+        if (circlesDistance(x, y, circlesX[i], circlesY[i]) < 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
 function draw(event) {
     var x = getPosition(event).x - canvas.offsetLeft;
     var y = getPosition(event).y - canvas.offsetTop;
-    drawCircle(x, y);
+    if (isNoCirclesNearby(x, y)) {
+        drawCircle(x, y);
+        circlesX.push(x);
+        circlesY.push(y);
+    }
 }
 
 function drawStart() {
+    document.addEventListener('click', draw);
     document.addEventListener('mousemove', draw);
 }
 
